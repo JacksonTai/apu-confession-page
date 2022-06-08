e54daffb8e8d502469b4e3d622cdbd63a3e0a6de
Jackson Tai <jacksontai21@gmail.com>
2022-06-08 17:16:39 +0800

    Complete basic page

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 const express = require('express')
 const app = express()
 const path = require('path')
-const ejsMate = require('ejs-mate');
 
-app.engine('ejs', ejsMate);
+// Middleware
+app.use(express.static(path.join(__dirname, "public")));
+
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 
-// Routes
+// Route
 app.use('/confessions', require('./routes/confessions'))
 
 // Routing
@@ -15,12 +16,16 @@ app.get('/', (req, res) => {
     res.render('home')
 })
 
-app.get('/about', (req, res) => {
-    res.render('about')
+app.get('/disclaimer', (req, res) => {
+    res.render('disclaimer')
 })
 
 app.get('/guidelines', (req, res) => {
     res.render('guidelines')
+})
+
+app.get('/information', (req, res) => {
+    res.render('information')
 })
 
 app.get('/apucp-admin', (req, res) => {
