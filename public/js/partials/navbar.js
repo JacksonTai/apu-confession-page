b5f9653fd13ce9c99a1fd39cdc80c9047802e812
Jackson Tai <jacksontai21@gmail.com>
2022-06-24 15:52:13 +0800

    Add toggleable theme color

--- public/js/partials/navbar.js
+++ public/js/partials/navbar.js
@@ -1,6 +1,21 @@
+const navbar = document.querySelector('.navbar')
 const navToggler = document.querySelector('.nav-toggler');
 const navbarList = document.querySelector('.navbar__list');
 
 navToggler.addEventListener('click', () => {
     navbarList.classList.toggle('active')
 })
+
+const navbarThemeBtn = document.querySelector('.navbar__theme-btn')
+navbarThemeBtn.addEventListener('click', () => {
+    toggleThemeLocalStorage()
+    toggleThemeBtn()
+    toggleRootClass()
+})
+
+const toggleThemeBtn = () => {
+    navbarThemeBtn.classList.toggle('fa-sun')
+    navbarThemeBtn.classList.toggle('fa-moon')
+}
+
+isDarkMode() ? toggleThemeBtn() : null;
