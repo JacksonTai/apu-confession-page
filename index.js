e413be25e683d784d44491b2beaa2c649c7ee109
Jackson Tai <74141823+JacksonTai@users.noreply.github.com>
2022-07-01 10:55:13 +0800

    Update index.js

--- index.js
+++ index.js
@@ -74,7 +74,7 @@ app.use(session({
     cookie: {
         name: 'session',
         httpOnly: true,
-        // secure: true,
+        secure: true,
         expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
         maxAge: 1000 * 60 * 60 * 24 * 7
     }
